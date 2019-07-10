ed7471476c04453f67cd0cb1f99d543d0d8eabe8
pepsi7959 <narongsak.mala@gmail.com>
2019-07-10 16:58:00 +0700

    features-answer-api

--- backend/components/surveys/index.js
+++ backend/components/surveys/index.js
@@ -32,7 +32,7 @@ router.get('/questions/:name', async function(req, res) {
     var model = new surveyDAL();
     console.log(req.params);
     if( !req.params.name ) {
-        res.status(200).json({
+        res.status(404).json({
             success: true,
             code: 404000,
             desc: "invalid uri",
@@ -54,6 +54,63 @@ router.post('/questions', async function(req, res) {
     var ctx = req.body;
     console.log(ctx);
     var r = await model.setQuestions(ctx);
+    res.status(200).json({
+        success: r.success,
+        code: 20000,
+        desc: "",
+        data: r.data })
+});
+
+router.get('/answers/:empid/:qid/:version', async function(req, res) {
+    
+    var model = new surveyDAL();
+    console.log(req.params);
+    if( !req.params.empid ) {
+        res.status(404).json({
+            success: true,
+            code: 404000,
+            desc: "invalid employee id",
+            data: null })
+        return;
+    }
+    if( !req.params.qid ) {
+        res.status(404).json({
+            success: true,
+            code: 404000,
+            desc: "invalid question id",
+            data: null })
+        return;
+    }
+    if( !req.params.version ) {
+        res.status(404).json({
+            success: true,
+            code: 404000,
+            desc: "invalid version",
+            data: null })
+        return;
+    }
+
+    var ctx = {
+        employeeId:req.params.empid,
+        name: req.params.qid,
+        version: req.params.version
+    }
+    logger.debug('query parameters: ' + JSON.stringify(ctx));
+    var data = await model.getAnswer(ctx);
+    console.log("data: " + data);
+    res.status(200).json({
+        success: true,
+        code: 20000,
+        desc: "",
+        data: data })
+});
+
+router.post('/answers', async function(req, res) {
+    
+    var model = new surveyDAL();
+    var ctx = req.body;
+    console.log(ctx);
+    var r = await model.setAnswer(ctx);
     res.status(200).json({
         success: r.success,
         code: 20000,
