1d33b0f6435dfbce3b8ed43de1864740d78e6fb0
srop <por.srop@gmail.com>
2019-08-22 15:19:55 +0700

    remove search , remove config ,change cover picture, change icon profile

--- client/src/layouts/components/navbar/navbar.js
+++ client/src/layouts/components/navbar/navbar.js
@@ -38,7 +38,7 @@ import userImage from "../../../assets/img/portrait/small/avatar-s-1.png";
 import userImage2 from "../../../assets/img/portrait/small/avatar-s-2.png";
 import userImage3 from "../../../assets/img/portrait/small/avatar-s-3.png";
 import userImage4 from "../../../assets/img/portrait/small/avatar-s-4.png";
-
+import userImagedga from "../../../assets/img/portrait/small/avatar-s-dga.png";
 class ThemeNavbar extends Component {
    handleClick = e => {
       this.props.toggleSidebarMenu("open");
@@ -90,9 +90,7 @@ class ThemeNavbar extends Component {
                      onClick={this.handleClick.bind(this)}
                      data-toggle="collapse"
                   />
-                  <Form className="navbar-form mt-1 float-left" role="search">
-                     <NavbarSearch />
-                  </Form>
+                 
                   {/* <Moon size={20} color="#333" className="m-2 cursor-pointer"/> */}
                   <MoreVertical
                      className="mt-1 navbar-toggler black no-border float-right"
@@ -109,7 +107,7 @@ class ThemeNavbar extends Component {
 
                         <UncontrolledDropdown nav inNavbar className="pr-1">
                            <DropdownToggle nav>
-                              <img src={userImage} alt="logged-in-user" className="rounded-circle width-35" />
+                              <img src={userImagedga} alt="logged-in-user" className="rounded-circle width-35" />
                            </DropdownToggle>
                            <DropdownMenu right>
                               <DropdownItem>
