bb265a2aa8f8f9bff50c1acf52b29c99925a4574
pepsi7959 <narongsak.mala@gmail.com>
2019-08-29 13:36:57 +0700

    support HTTP OPTIONS

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,16 @@ app.use(morgan('combined', { stream: accessLogStream }))
 
 app.use(express.json())
 
+/**
+ * Support OPTIONS
+ */
+app.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
+
 app.use('/v1/auth', auth);
 app.use('/v1/users', user);
 app.use('/v1/survey', survey);
