d2cac76517139c741fafa6ef875377c4b41cf897
srop <por.srop@gmail.com>
2019-09-06 09:03:32 +0700

    error sidemenubar at my chrome

--- client/src/layouts/components/sidebar/sidemenu/sidemenu.js
+++ client/src/layouts/components/sidebar/sidemenu/sidemenu.js
@@ -1,7 +1,7 @@
 // import external modules
 import React, { Component } from "react";
-import { Link,Redirect } from "react-router-dom";
-
+import { Link,Redirect,withRouter } from "react-router-dom";
+import login from '../../../../views/pages/login';
 import {
    Home,
    Mail,
@@ -29,9 +29,58 @@ import "../../../../assets/scss/components/sidebar/sidemenu/sidemenu.scss";
 // import internal(own) modules
 import SideMenu from "../sidemenuHelper";
 
+
 class SideMenuContent extends Component {
+   constructor(props) {
+      super(props);
+  
+     
+      this.state = {
+       
+        redirectToReferrer: false
+     };
+      
+      //this.Auth = new AuthService();
+   }
+
+   componentDidMount(){
+      fetch("http://164.115.17.101:8082/v1/survey/questions/seminar-01", {
+         method: 'get',
+         crossDomain: true,
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json',
+           "userid": localStorage.getItem("session_userid"),
+           "token": localStorage.getItem("token_local")
+         // "token" : "3gUMtyWlKatfMk5aLi5PpgQxfTJcA91YlN6Nt8XyiR1CwLs6wGP69FSQs8EKHCsg",
+         },
+       
+       })
+
+       .then((response) => {
+         console.log(response.status); 
+             if (response.status !== 200) {
+               //this.setState({redirectToReferrer:false});
+             console.log('chkredirect==>'+this.state.redirectToReferrer);
+          //   this.props.history.push('/login');
+           }
+               return response.json();
+         })
+     
+   
+       .then(res => {
+      
+         if(res.data){
+           this.setState({redirectToReferrer:true});
+          
+           this.setState({json:(res.data)});
+           console.log("componentDidMount 2");
+           }
+       })
+   }
    render() {
-      if (localStorage.getItem('token_local') === null) {
+      if (localStorage.getItem('token_local') === null || this.state.redirectToReferrer === false) {
+         console.log('cannto login')
          return (<Redirect to={'login'}/>)
       }
       let dataList = JSON.parse(localStorage.getItem("userData"));
