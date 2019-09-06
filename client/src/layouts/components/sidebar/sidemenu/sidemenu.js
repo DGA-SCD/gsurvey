3c883a54e0c25c83a3b42953d0a482b9d40a5579
srop <por.srop@gmail.com>
2019-09-06 10:04:05 +0700

    fix bug sidemenu

--- client/src/layouts/components/sidebar/sidemenu/sidemenu.js
+++ client/src/layouts/components/sidebar/sidemenu/sidemenu.js
@@ -1,6 +1,6 @@
 // import external modules
 import React, { Component } from "react";
-import { Link,Redirect } from "react-router-dom";
+import { Link,Redirect,NavLink } from "react-router-dom";
 
 import {
    Home,
@@ -22,7 +22,7 @@ import {
    LifeBuoy,
    Users
 } from "react-feather";
-import { NavLink } from "react-router-dom";
+
 
 // Styling
 import "../../../../assets/scss/components/sidebar/sidemenu/sidemenu.scss";
@@ -32,90 +32,106 @@ import SideMenu from "../sidemenuHelper";
 
 class SideMenuContent extends Component {
   
- 
+  
    render() {
+     
       if (localStorage.getItem('token_local') === null ) {
        
          return (<Redirect to={'login'}/>)
-      }
-      let dataList = JSON.parse(localStorage.getItem("userData"));
-      if(dataList.role === 'admin'){
-      return (
-         <SideMenu className="sidebar-content" toggleSidebarMenu={this.props.toggleSidebarMenu}>
-           
-            
-            <SideMenu.MenuSingleItem>
-               <NavLink to="/pages/survey" activeClassName="active">
-                  <i className="menu-icon">
-                     <CheckSquare size={18} />
-                  </i>
-                  <span className="menu-item-text">แบบสำรวจ</span>
-               </NavLink>
-            </SideMenu.MenuSingleItem>
-            
-            <SideMenu.MenuSingleItem>
-               <NavLink to="/pages/SurveyCreator" activeClassName="active">
-                  <i className="menu-icon">
-                     <Layers size={18} />
-                  </i>
-                  <span className="menu-item-text">Create SurveyForm</span>
-               </NavLink>
-            </SideMenu.MenuSingleItem>
-            }
-            <SideMenu.MenuSingleItem>
-               <NavLink to="/pages/summary" activeClassName="active">
-                  <i className="menu-icon">
-                     <MessageSquare size={18} />
-                  </i>
-                  <span className="menu-item-text">Datatable</span>
-               </NavLink>
-            </SideMenu.MenuSingleItem>
-            <SideMenu.MenuSingleItem>
-               <NavLink to="/pages/surveyresult" activeClassName="active">
-                  <i className="menu-icon">
-                     <Copy size={18} />
-                  </i>
-                  <span className="menu-item-text">ผลสำรวจ</span>
-               </NavLink>
-            </SideMenu.MenuSingleItem>
-            
+      }else{
 
-            
-           
-         </SideMenu>
-      );
-      }
-      if(dataList.role === 'user'){
-         return (
-            <SideMenu className="sidebar-content" toggleSidebarMenu={this.props.toggleSidebarMenu}>
-              
-               
-               <SideMenu.MenuSingleItem>
-                  <NavLink to="/pages/survey" activeClassName="active">
-                     <i className="menu-icon">
-                        <CheckSquare size={18} />
-                     </i>
-                     <span className="menu-item-text">แบบสำรวจ</span>
-                  </NavLink>
-               </SideMenu.MenuSingleItem>
-               
-              
-               
-               <SideMenu.MenuSingleItem>
-                  <NavLink to="/pages/surveyresult" activeClassName="active">
-                     <i className="menu-icon">
-                        <Copy size={18} />
-                     </i>
-                     <span className="menu-item-text">ผลสำรวจ</span>
-                  </NavLink>
-               </SideMenu.MenuSingleItem>
-               
+      
+         if(JSON.parse(localStorage.getItem("userData") === undefined)){
+            return (<Redirect to={'login'}/>)
+         }else{
+
+           let dataList = JSON.parse(localStorage.getItem("userData"));
+
+            if(dataList.role === 'admin'){
    
-               
-              
-            </SideMenu>
-         );
+               return (
+                  <SideMenu className="sidebar-content" toggleSidebarMenu={this.props.toggleSidebarMenu}>
+                    
+                     
+                     <SideMenu.MenuSingleItem>
+                        <NavLink to="/pages/survey" activeClassName="active">
+                           <i className="menu-icon">
+                              <CheckSquare size={18} />
+                           </i>
+                           <span className="menu-item-text">แบบสำรวจ</span>
+                        </NavLink>
+                     </SideMenu.MenuSingleItem>
+                     
+                     <SideMenu.MenuSingleItem>
+                        <NavLink to="/pages/SurveyCreator" activeClassName="active">
+                           <i className="menu-icon">
+                              <Layers size={18} />
+                           </i>
+                           <span className="menu-item-text">Create SurveyForm</span>
+                        </NavLink>
+                     </SideMenu.MenuSingleItem>
+                     }
+                     <SideMenu.MenuSingleItem>
+                        <NavLink to="/pages/summary" activeClassName="active">
+                           <i className="menu-icon">
+                              <MessageSquare size={18} />
+                           </i>
+                           <span className="menu-item-text">Datatable</span>
+                        </NavLink>
+                     </SideMenu.MenuSingleItem>
+                     <SideMenu.MenuSingleItem>
+                        <NavLink to="/pages/surveyresult" activeClassName="active">
+                           <i className="menu-icon">
+                              <Copy size={18} />
+                           </i>
+                           <span className="menu-item-text">ผลสำรวจ</span>
+                        </NavLink>
+                     </SideMenu.MenuSingleItem>
+            
+                  </SideMenu>
+               );
+               }
+               else if(dataList.role === 'user'){
+                  return (
+                     <SideMenu className="sidebar-content" toggleSidebarMenu={this.props.toggleSidebarMenu}>
+                       
+                        
+                        <SideMenu.MenuSingleItem>
+                           <NavLink to="/pages/survey" activeClassName="active">
+                              <i className="menu-icon">
+                                 <CheckSquare size={18} />
+                              </i>
+                              <span className="menu-item-text">แบบสำรวจ</span>
+                           </NavLink>
+                        </SideMenu.MenuSingleItem>
+                        
+                       
+                        
+                        <SideMenu.MenuSingleItem>
+                           <NavLink to="/pages/surveyresult" activeClassName="active">
+                              <i className="menu-icon">
+                                 <Copy size={18} />
+                              </i>
+                              <span className="menu-item-text">ผลสำรวจ</span>
+                           </NavLink>
+                        </SideMenu.MenuSingleItem>
+                        
+            
+                        
+                       
+                     </SideMenu>
+                  );
+                  }
+               else{
+                  return (<Redirect to={'login'}/>)
+               }
+
+
          }
+
+      }
+ 
+      
    }
 }
 
