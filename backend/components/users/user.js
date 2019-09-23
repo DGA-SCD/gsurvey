0d3af9c35b769590a8de085938595b48ddea419b
pepsi7959 <narongsak.mala@gmail.com>
2019-09-23 15:27:38 +0700

    revise user api

--- backend/components/users/user.js
+++ backend/components/users/user.js
@@ -27,11 +27,80 @@ const mysql = require('mysql');
 const winston = require('../../commons/logger');
 const http = require('../../commons/http');
 const appConf = require('../../config/production.conf');
+const schema = require('./allBookingSchema');
 const redis = require("redis");
 const logger = winston.logger;
 const router = express.Router();
 const promise = require('promise');
 
+// Get User profile 
+function getProfile(req, res){
+    
+    var userName = req.USERID;
+    var conn = mysql.createConnection({
+        host: appConf.MYSQL_host,
+        port: appConf.MYSQL_port,
+        user: appConf.MYSQL_user,
+        password: appConf.MYSQL_password,
+        database: appConf.MYSQL_database
+    });
+    
+    conn.connect(function(err) {
+        
+        logger.debug("Trying...");
+
+        if ( err ) {
+            logger.error("Cannot connect to mariadb");
+            http.error(res, 500, 50000, "Cannot connect to mariadb");
+            conn.end();
+            return;
+        }else{
+            logger.debug("Database connected!");
+            logger.debug("Search user name: " + userName);
+
+            const qstr = "SELECT * FROM user_details \
+            WHERE user_details.UserId = " + userName;
+            //INNER JOIN passwords on user_details.UserId = passwords.Username \
+            //WHERE passwords.username = '" + userName + "' and passwords.password = '"+ password +"'";
+
+            conn.query(qstr, function(err, result, fields){
+                if ( err ) {
+                    logger.error( err );
+                    http.error(res, 404, 404000, "Not found user or password");
+                    conn.end();
+                    return; 
+                } else {
+                    logger.debug( "result " + JSON.stringify(result));
+                    var userDetails = result[0];
+                    if ( result[0] != null ) {
+                        http.success(res, { 
+                            id: userDetails.UserID,
+                            name: userDetails.Name,
+                            surname: userDetails.Surname,
+                            nameEN: userDetails.NameEn,
+                            surnameEN: userDetails.SurnameEn,
+                            nickName: userDetails.NickName,
+                            tel: userDetails.Telephone,
+                            email: userDetails.Email,
+                            position: userDetails.Position,
+                            department: userDetails.Department,
+                            devision: userDetails.Segment,
+                            level: userDetails.Level,
+                            role: userDetails.Role
+                        })
+                        conn.end();
+                    } else {
+                        logger.error("invalid user or password");
+                        http.error(res, 401, 401000, "invalid user or password");
+                        conn.end();
+                    }
+                }
+            });
+        }
+    });
+}
+
+// Get roomate by userid
 function getRoommatesByUserId(req, res){
 
     var userId = req.params.userid;
@@ -348,31 +417,114 @@ function getConnection(){
     });
 }
 
+// getAlluser
+function getAllUser(conn){
+
+    const qstr = "SELECT \
+        t.*,\
+        u2.Name  as FName,\
+        u2.Surname as FSurname \
+    FROM ( \
+        SELECT   \
+            u.userID, \
+            u.Name, \
+            u.Surname, \
+            u.Department, \
+            u.Segment, \
+            r.FriendID FROM user_details as u \
+    LEFT JOIN roommates as r \
+    ON u.userId = r.UserID \
+    ) as t \
+    LEFT JOIN user_details as u2 on t.FriendID = u2.UserID";
+
+    return new promise(function(resolve, reject) {
+        conn.query(qstr, function(err, result, fields){
+            if ( err ) {
+                logger.error( "query failed: " + err );
+                reject( err );
+            }
+            else {
+                conn.end();
+                logger.debug( "qeury result: " + JSON.stringify( result ) );
+                resolve( result );
+            }
+        });
+    });
+}
+
+// mysql connection
+function getConnection(){
+    var conn = mysql.createConnection({
+        host: appConf.MYSQL_host,
+        port: appConf.MYSQL_port,
+        user: appConf.MYSQL_user,
+        password: appConf.MYSQL_password,
+        database: appConf.MYSQL_database
+    });
+    return new promise(function(resolve, reject){
+        conn.connect(function(err) {
+            if ( err ) {
+                logger.error( "mysql connect failed" );
+                reject( err );
+            } else {
+                logger.debug( "mysql connected" );
+                resolve( conn );
+            }
+        })
+    });
+}
+
 // Get list booking 
 function getAllBooking(req, res){
 
-    let mysql = getConnection();
+    let mysqlConn = getConnection();
 
     return new promise((resolve, reject) => {
 
-         mysql.catch( err => {
+        mysqlConn.catch( err => {
             http.error(res, 500, 500100, "connect to mysql failed: " + err);
             reject( err );
         })
-         .then( mysqlConn =>{
-            return getAllUser(mysqlConn);
+        .then( conn =>{
+            return getAllUser(conn);
          })    
-         .catch( err => {
+        .catch( err => {
             http.error(res, 500, 500200, "cannot query data from mariadb: " + err);
             reject( err );
          }).then( allUsers => {
-            console.log(allUsers);
-            resolve(allUsers);
-         })
+            console.log("affected row: " + allUsers.length);
+            var lists = [];
+            allUsers.forEach(e => {
+                
+                let fullFriendName = "";
+
+                if ( e.FriendID != null ){
+                    fullFriendName = e.FName + " " + e.FSurname;
+                }
+
+                lists.push({
+                    userId: e.UserID,
+                    fullname: e.Name + " " + e.Surname,
+                    department: e.Department,
+                    segment: e.Segment,
+                    friend: fullFriendName,
+                    room: 0,
+                    vehicle: 0,
+                    remark: "",
+                });
+            });
+            http.success(res, {
+                columns: schema.allbooking(),
+                data: lists
+            });
+            mysqlConn.end();
+            resolve(true);
+         });
     }); 
 }
 
 module.exports = {
+    getProfile,
     getRoommatesByUserId,
     getRoommates,
     bindRoommate,
