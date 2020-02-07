cf51eeb32d3688cb4580fa13ffa797e0a46765ca
pepsi7959 <narongsak.mala@gmail.com>
2020-02-07 17:21:09 +0700

    support rename and delete a survey

--- backend/components/helpers/mongodb.js
+++ backend/components/helpers/mongodb.js
@@ -4,26 +4,120 @@ const appConf = require('../../config/production.conf');
 const logger = winston.logger;
 const promise = require('promise');
 
-await MongoClient.connect(appConf.mongoDB, { useNewUrlParser: true })
-    .then(  async function(db){
-            logger.info("successfully connected MongoDB");
-            await db.db(appConf.MONGODB_dbname).collection('answer').findOne(ctx, function (error, response) {
-                if(error) {
-                    console.log('Error occurred while inserting');
-                } else {
-                   console.log('response: ', response);
-                   data = response;
-                }
-                
+function find(filters, collection) {
+    return new promise((resolve, reject) => {
+        MongoClient.connect(appConf.mongoDB, {
+                useNewUrlParser: true
+            }).then(db => {
+                logger.debug("mongodb connected");
+                db.db(appConf.MONGODB_dbname)
+                    .collection(collection).find(filters).toArray(function (err, results) {
+                        if (err) {
+                            logger.error('Error occurred while querying: ' + err);
+                            db.close();
+                            reject(err);
+                        } else {
+                            console.log(results);
+                            logger.debug('results: ' + JSON.stringify(results));
+                            db.close();
+                            resolve(results);
+                        }
+                    })
+            })
+            .catch(function (err) {
+                logger.error("Failed to connection MongoDB:" + err);
+                reject(err);
             });
-            await db.close();
-    }).catch( function(err){
-        logger.error("Failed to connection MongoDB");
-    });
-    console.log(data);
-    return data;
+    })
 }
 
-module exports = {
-    getConnection
+function insert(filters, data, collection) {
+    return new promise((resolve, reject) => {
+        MongoClient.connect(appConf.mongoDB, {
+                useNewUrlParser: true
+            }).then(db => {
+                logger.debug("mongodb connected");
+                db.db(appConf.MONGODB_dbname)
+                    .collection(collection).findOneAndReplace(filters, data, {
+                        upsert: true
+                    }, function (err, result) {
+                        if (err) {
+                            logger.error('Error occurred while inserting: ' + err);
+                            db.close();
+                            reject(err);
+                        } else {
+                            logger.debug('inserted record: ' + JSON.stringify(result));
+                            db.close();
+                            resolve(true);
+                        }
+                    })
+            })
+            .catch(function (err) {
+                logger.error("Failed to connection MongoDB:" + err);
+                reject(err);
+            });
+    })
+}
+
+function update(filters, data, collection) {
+    return new promise((resolve, reject) => {
+        MongoClient.connect(appConf.mongoDB, {
+                useNewUrlParser: true
+            }).then(db => {
+                logger.debug("mongodb connected");
+                db.db(appConf.MONGODB_dbname)
+                    .collection(collection).updateOne(filters, data, {
+                        upsert: false
+                    }, function (err, result) {
+                        if (err) {
+                            logger.error('Error occurred while updating: ' + err);
+                            db.close();
+                            reject(err);
+                        } else {
+                            logger.debug('updated record: ' + JSON.stringify(result));
+                            db.close();
+                            resolve(true);
+                        }
+                    })
+            })
+            .catch(function (err) {
+                logger.error("Failed to connection MongoDB:" + err);
+                reject(err);
+            });
+    })
+}
+
+function remove(filters, justOne, collection) {
+    return new promise((resolve, reject) => {
+        MongoClient.connect(appConf.mongoDB, {
+                useNewUrlParser: true
+            }).then(db => {
+                logger.debug("mongodb connected");
+                db.db(appConf.MONGODB_dbname)
+                    .collection(collection).deleteOne(filters, function (err, results) {
+                        if (err) {
+                            logger.error('Error occurred while querying: ' + err);
+                            db.close();
+                            reject(err);
+                        } else {
+                            console.log(results);
+                            logger.debug('results: ' + JSON.stringify(results));
+                            db.close();
+                            resolve(results);
+                        }
+                    })
+            })
+
+            .catch(function (err) {
+                logger.error("Failed to connection MongoDB:" + err);
+                reject(err);
+            });
+    })
+}
+
+module.exports = {
+    find,
+    insert,
+    update,
+    remove
 }
