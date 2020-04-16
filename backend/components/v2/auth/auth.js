92eb3793508bf344c9ff3827db5b8972e33231e0
pepsi7959 <narongsak.mala@gmail.com>
2020-04-16 12:56:48 +0700

    v2-backend-revise-error-code

--- backend/components/v2/auth/auth.js
+++ backend/components/v2/auth/auth.js
@@ -19,47 +19,38 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+"use strict";
 
-const express = require('express');
 const cookieParser = require('cookie-parser');
 const crypto = require('crypto');
 const base64url = require('base64url')
-const mysql = require('mysql');
 const promise = require('promise');
-const winston = require('../../../commons/logger');
+const {
+    logger
+} = require('../../../commons/logger');
 const http = require('../../../commons/http');
 const appConf = require('../../../config/production.conf');
-const redis = require("redis");
 var passHash = require('password-hash');
-const logger = winston.logger;
-const router = express.Router();
+const mysqlHelper = require('../../helpers/mysql');
+const redisHelper = require('../../helpers/redis');
 
+function login(req, res) {
 
-// Login 
-// require username and password
+    var ctx = req.body;
+    const username = ctx.username;
 
-function getConnection() {
-    var conn = mysql.createConnection({
-        host: appConf.MYSQL_host,
-        port: appConf.MYSQL_port,
-        user: appConf.MYSQL_user,
-        password: appConf.MYSQL_password,
-        database: appConf.MYSQL_database
-    });
-    return new promise(function (resolve, reject) {
-        conn.connect(function (err) {
-            if (err) {
-                logger.error("mysql connect failed");
-                reject(err);
-            } else {
-                logger.debug("mysql connected");
-                resolve(conn);
-            }
-        })
-    });
-}
+    if (username === undefined) {
+        http.error(res, 401, 401000, "not found username");
+        return;
+    }
 
-function getUserDetails(conn, userName, password) {
+    const password = ctx.password;
+    if (password === undefined) {
+        http.error(res, 401, 401000, "not found password");
+        return;
+    }
+
+    var token = base64url(crypto.randomBytes(48));
 
     const qstr = "SELECT * " +
         "FROM users " +
@@ -67,188 +58,109 @@ function getUserDetails(conn, userName, password) {
         "JOIN department on department.dep_id = user_profile.dep_id " +
         "JOIN ministry on ministry.ministry_code = user_profile.ministry_code " +
         "JOIN roles on roles.role_id = users.role_id " +
-        "WHERE users.username = '" + userName + "' and user_profile.delete_flag = 0";
+        "WHERE users.username = '" + username + "'";
 
-    return new promise(function (resolve, reject) {
-        conn.query(qstr, function (err, result, fields) {
-            if (err) {
-                logger.error("query failed: " + err);
-                reject(err);
-            } else {
-                conn.end();
-                if( result.length > 0 && result[0].approval_status !== undefined && result[0].approval_status === "waiting") {
-                    reject({code: "UNAPPROVED"});
-                }else{
-                    resolve(result[0]);
-                }
-            }
-        });
-    });
-}
-
-function getRedisConnection() {
-
-    const redisCli = redis.createClient({
-        port: appConf.REDIS_port, // replace with your port
-        host: appConf.REDIS_host, // replace with your hostanme or IP address
-        password: appConf.REDIS_pass,
-    });
-
-    return new promise(function (resolve, reject) {
-        redisCli.on("error", function (err) {
-            if (err) {
-                logger.error("redis: " + err);
-                reject(err);
-            }
-        });
-
-        redisCli.on("connect", function () {
-            resolve(redisCli);
-        });
-    });
-}
-
-function redisSet(redisCli, key, value, exp) {
-    let expire = exp !== undefined ? exp : 3600;
-    logger.debug("Begin to set value on redis " + "key=[" + key + "]" + "value=[" + value + "]");
-    return new promise(function (resolve, reject) {
-        redisCli.set(key, value, 'EX', expire, function (err, reply) {
-            if (err) {
-                logger.error("redis: " + err);
-                reject(err);
-            } else {
-                logger.debug("redis: " + reply);
-                resolve(reply);
-            }
-        });
-    });
-}
-
-function redisGet(redisCli, key) {
-    return new promise(function (resolve, reject) {
-        redisCli.get(key, function (err, reply) {
-            if (err) {
-                logger.error("redis: " + err);
-                reject(err);
-            } else {
-                logger.debug("redis: " + reply);
-                resolve(reply);
-            }
-        });
-    });
-}
-
-function login(req, res) {
-
-    var ctx = req.body;
-    const username = ctx.username;
-
-    if( username === undefined ){
-        http.error(res, 401, 401000, "not found username");
-        return;
-    }
-
-    const password = ctx.password;
-    if( password === undefined ){
-        http.error(res, 401, 401000, "not found password");
-        return;
-    }
-
-    var token = base64url(crypto.randomBytes(48));
-
-    let mysql = getConnection();
-    let redis;
-    var userDetails;
-
-    return new promise(function (resolve, reject) {
-        mysql.catch(err => {
-                http.error(res, 500, 500100, "connect to mysql failed: " + err);
-                reject(err);
-            })
-            .then((mysqlConn) => {
-                return getUserDetails(mysqlConn, username, password);
-            })
-            .catch(err => {
-                if( err.code !== undefined && err.code === "UNAPPROVED") {
-                    http.error(res, 401, 401001, "This user is waiting for approval");
-                }else{
-                    http.error(res, 500, 500200, "query mysql failed: " + err);
-                }
-                reject(err);
-            })
-            .then(usrDetails => {
-                if (usrDetails === undefined) {
-                    logger.error("Not found " + username);
-                    http.error(res, 404, 404000, "not found an user");
-                    reject("Not found " + username);
-                } else {
-                    logger.debug("User Profile: " + JSON.stringify(usrDetails));
-                    logger.debug("verify password: " + passHash.verify(password, usrDetails.password));
-                    if (passHash.verify(password, usrDetails.password)) {
-                        userDetails = usrDetails;
-                        logger.debug("User Details: " + JSON.stringify(usrDetails));
-                        return getRedisConnection();
-                    } else {
-                        http.error(res, 401, 401000, "invalid password");
-                        reject("Not found " + username);
+    mysqlHelper.getConnection()
+        .then(conn => {
+            return mysqlHelper.query(conn, qstr)
+                .then(results => {
+                    
+                    let compose_error = (h, e, d) => {
+                        return {
+                            httpCode: h,
+                            errorCode: e,
+                            desc: d
+                        };
                     }
-                }
-            })
-            .catch(err => {
-                reject(err);
-                http.error(res, 500, 500100, "connect to redis failed: " + err);
-            })
-            .then(redisConn => {
-                return redisSet(redisConn, username, token, 60 * appConf.tokenExpiredTime);
-            })
-            .catch(err => {
-                reject(err);
-                http.error(res, 500, 500200, "set value on redis failed: " + err);
-            })
-            .then(reply => {
+                    
+                    if (results.length > 0) {
 
-                let options = {
-                    maxAge: 1000 * 60 * appConf.tokenExpiredTime, // would expire after 15 minutes
-                    httpOnly: true, // The cookie only accessible by the web server
-                    signed: appConf.cookies.signed // Indicates if the cookie should be signed
-                }
+                        logger.debug("user profile: " + JSON.stringify(results));
 
-                res.cookie('userid', userDetails.user_id, options);
-                res.cookie('username', userDetails.username, options);
-                res.cookie('token', token, options);
-
-                http.success(res, {
-                    userid: userDetails.user_id,
-                    user: userDetails.username,
-                    name: userDetails.firstname,
-                    surname: userDetails.lastname,
-                    nameEN: "",
-                    surnameEN: "",
-                    nickName: "",
-                    tel: userDetails.office_phone,
-                    ext: userDetails.ext,
-                    mobile: userDetails.mobile,
-                    email: userDetails.email,
-                    position: userDetails.position,
-                    organize: userDetails.org_name,
-                    department: userDetails.dep_name,
-                    devision: "",
-                    role: userDetails.role_name
+                        if (!passHash.verify(password, results[0].password)) {
+                            conn.destroy();
+                            return Promise.reject(compose_error(400, 40000, "invalid password"));
+                        } else if (results[0].delete_flag !== undefined && results[0].delete_flag == 1) {
+                            conn.destroy();
+                            return Promise.reject(compose_error(401, 40105, "this user was deleted"));
+                        } else if (results[0].suspended_flag !== undefined && results[0].suspended_flag == 1) {
+                            conn.destroy();
+                            return Promise.reject(compose_error(401, 40104, "this user was disabled"));
+                        } else if (results[0].approval_status !== undefined && results[0].approval_status === "reject") {
+                            conn.destroy();
+                            return Promise.reject(compose_error(401, 40103, "this user was rejected"));
+                        } else if (results[0].approval_status !== undefined && results[0].approval_status === "waiting") {
+                            conn.destroy();
+                            return Promise.reject(compose_error(401, 40106, "this user is waiting for approval"));
+                        } else if (results[0].approval_status !== undefined && !(results[0].approval_status === "waiting" || results[0].approval_status === "reject" || results[0].approval_status === "approve")) {
+                            conn.destroy();
+                            return Promise.reject(compose_error(500, 50000, "unknown approval status: " + results[0].approval_status));
+                        } else {
+                            conn.destroy();
+                            return Promise.resolve(results[0]);
+                        }
+                    } else {
+                        conn.destroy();
+                        return Promise.reject(compose_error(404, 40401, "not found an user"));
+                    }
                 });
-                resolve(true);
-            })
-            .catch(err => {
-                http.error(res, 500, 50002, "server error: " + err);
-                reject(err)
+
+        })
+        .then(usrDetails => {
+            return redisHelper.getRedisConnection()
+                .then(redisCli => {
+                    return redisHelper.redisSet(redisCli, username, token, 60 * appConf.tokenExpiredTime)
+                        .then(reply => {
+                            return Promise.resolve(usrDetails);
+                        })
+                        .catch(err => {
+                            return Promise.reject({
+                                httpCode: 500,
+                                errorCode: 50002,
+                                desc: "cannot write token on redis: " + err
+                            });
+                        });
+                });
+        })
+        .then(userDetails => {
+            let options = {
+                maxAge: 1000 * 60 * appConf.tokenExpiredTime, // would expire after 15 minutes
+                httpOnly: true, // The cookie only accessible by the web server
+                signed: appConf.cookies.signed // Indicates if the cookie should be signed
+            }
+
+            res.cookie('userid', userDetails.user_id, options);
+            res.cookie('username', userDetails.username, options);
+            res.cookie('token', token, options);
+
+            http.success(res, {
+                userid: userDetails.user_id,
+                user: userDetails.username,
+                name: userDetails.firstname,
+                surname: userDetails.lastname,
+                nameEN: "",
+                surnameEN: "",
+                nickName: "",
+                tel: userDetails.office_phone,
+                ext: userDetails.ext,
+                mobile: userDetails.mobile,
+                email: userDetails.email,
+                position: userDetails.position,
+                organize: userDetails.org_name,
+                department: userDetails.dep_name,
+                devision: "",
+                role: userDetails.role_name
             });
-    });
+        })
+        .catch(err => {
+            if (err.httpCode !== undefined) {
+                http.error(res, err.httpCode, err.errorCode, err.desc);
+            } else {
+                http.error(res, 500, 50002, err);
+            }
+        });
 }
 
 module.exports = {
-    login,
-    getConnection,
-    getUserDetails,
-    getRedisConnection,
-    redisGet,
-    redisSet
+    login
 }
